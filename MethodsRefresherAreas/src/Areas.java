94e06c56ff4f6af52c7ae5d673c25dc5271ac32e
QiChang Liu <qcl@QiChangdeAir.attlocal.net>
2021-02-27 00:31:06 -0800

    update comments

---
+++ MethodsRefresherAreas/src/Areas.java
@@ -0,0 +1,68 @@
+/**
+ * Methods Refresher Lab - Areas class.
+ *
+ * Copyright (c) [2021], Zhao Liu.
+*/
+
+import java.lang.Math;
+
+/**
+ * Methods of calculating the area different different shapes, each method takes
+ * in single or multiple arguments.  
+ */
+public class Areas {
+  /**
+   * Method calculates the area of a circle.
+   * @param radius: length of the circle
+   * @param PI: math constant 
+   * @return area of a circle
+   */
+  public double areaCircle(double radius, final double PI) {
+    double circleArea = PI * Math.pow(radius, 2);
+    return circleArea;
+  }
+
+  /**
+   * Method calculates the area of a square.
+   * @param length: length of a square's four sides
+   * @return area of a square
+   */
+  public double areaSquare(double length) {
+    double squareArea = Math.pow(length, 2);
+    return squareArea;
+  }
+
+  /**
+   * Method calculates the area of a triangle.
+   * @param length: length of a triangle's base
+   * @param height: height of a triangle
+   * @return area of a triangle
+   */
+  public double areaTriangle(double length, double height) {
+    double triangleArea = length * height / 2;
+    return triangleArea;
+  }
+
+  /**
+   * Method calculates the area of a rectangle.
+   * @param length: length of a rectangle's long side
+   * @param width: length of a rectangle's short side
+   * @return area of a rectangle
+   */
+  public double areaRectangle(double length, double width) {
+    double rectangleArea = length * width;
+    return rectangleArea;
+  }
+
+  /**
+   * Method calculates the area of a trapezoid.
+   * @param upper: length of a trapezoid's top side 
+   * @param lower: length of a trapezoid's bottom side 
+   * @param height: height of a trapezoid
+   * @return area of a trapezoid
+   */
+  public double areaTrapezoid(double upper, double lower, double height) {
+    double trapezoidArea = (upper + lower) * height / 2;
+    return trapezoidArea;
+  }
+}
---
+++ MethodsRefresherAreas/src/CalculateAreas.java
@@ -0,0 +1,58 @@
+/**
+ * Methods Refresher Lab - CalculateAreas class
+ *
+ * Copyright (c) [2021], Zhao Liu.
+*/
+
+/**
+ * A CalculateAreas class has intialized variable to calculate the actual area
+ * of different shapes.
+ */
+public class CalculateAreas {
+  // Constant PI in math
+  final static double PI_MATH = 3.1415;
+
+  /**
+   * Main method.
+   * @param args
+   */
+  public static void main(String[] args) {
+    // Construct a new Areas object
+    Areas area = new Areas();
+
+    // Create and initialize variables for the appropriate sides
+    double circleRadius = 1.1;
+    double squareSideLength = 2.2;
+    double triangleLength = 3.3, triangleHeight = 4.4;
+    double rectangleLength = 5.5, rectangleWidth = 6.6;
+    double trapezoidTop = 7.7, trapezoidBottom = 8.8, trapezoidHeight = 9.9;
+
+    // Initialize variables for the 5 variables from the Areas class
+    double circleArea = area.areaCircle(circleRadius, PI_MATH);
+    double squareArea = area.areaSquare(squareSideLength);
+    double triangleArea = area.areaTriangle(triangleLength, triangleHeight);
+    double rectangleArea = area.areaRectangle(rectangleLength, rectangleWidth);
+    double trapezoidArea = area.areaTrapezoid(trapezoidTop, trapezoidBottom,
+                                           trapezoidHeight);
+    
+    // Call the method in Output class to print out the 5 areas
+    Output.outputResult(circleArea);                              
+    Output.outputResult(squareArea);                              
+    Output.outputResult(triangleArea);                              
+    Output.outputResult(rectangleArea);                              
+    Output.outputResult(trapezoidArea);                              
+  }
+}
+
+/**
+ * A Output class has the method to output the area computed.
+ */
+class Output {
+  /**
+   * Method prints out the area computed
+   * @param area: result of computed area
+   */
+  public static void outputResult(double area) {
+    System.out.println(area);
+  }
+}
